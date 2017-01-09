54400f12c4ff8a1f6a33388d840e3bb2b2adfc3b
jordin francis <jordinfrancis@students.ptechnyc.org>
2017-01-09 15:06:37 +0000

    grid

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Write your function calls below this line
+grid function
+setColor(2,0,"purple");
+setColor(2,4,"purple");
+setColor(4,8,"blue");
+setColor(4,9,"blue");
+setColor(0,0,"green");
+setColor(0,4, "green");
+setColor(4,0,"red");
+setColor(4,4,"red");
